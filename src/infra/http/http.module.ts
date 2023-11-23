552f7efc4829aec31c22c100483fe1acb63f6b47
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-11-23 13:57:43 -0300

    Comunicação entre camadas

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,6 +5,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { DatabaseModule } from '../database/database.module'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 
 @Module({
   imports: [DatabaseModule],
@@ -13,6 +14,7 @@ import { DatabaseModule } from '../database/database.module'
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController
-  ]
+  ],
+  providers: [CreateQuestionUseCase]
 })
 export class HttpModule {}
