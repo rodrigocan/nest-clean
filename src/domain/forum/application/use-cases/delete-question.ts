a4f8ee5cd7244913eaf7e0b97017d85187e1af34
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-02 16:13:54 -0300

    Controller: Deletar Pergunta

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from '@/core/either'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 
 interface DeleteQuestionUseCaseRequest {
   authorId: string
@@ -13,12 +14,13 @@ type DeleteQuestionUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
   async execute({
     authorId,
-    questionId
+    questionId,
   }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId)
 
