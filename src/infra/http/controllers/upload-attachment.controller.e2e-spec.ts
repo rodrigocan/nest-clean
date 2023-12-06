d9308af9f9ddc2cd7ffb59209d0f7ee0e6f68a75
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-06 19:59:05 -0300

    Testando controller de upload

--- src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -1,40 +1,43 @@
-import { AppModule } from "@/infra/app.module";
-import { DatabaseModule } from "@/infra/database/database.module";
-import { INestApplication } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { Test } from "@nestjs/testing";
-import request from "supertest";
-import { StudentFactory } from "test/factories/make-student";
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
 
-describe("Upload attachment (E2E)", () => {
-  let app: INestApplication;
-  let studentFactory: StudentFactory;
-  let jwt: JwtService;
+describe('Upload attachment (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
       providers: [StudentFactory],
-    }).compile();
+    }).compile()
 
-    app = moduleRef.createNestApplication();
+    app = moduleRef.createNestApplication()
 
-    studentFactory = moduleRef.get(StudentFactory);
-    jwt = moduleRef.get(JwtService);
+    studentFactory = moduleRef.get(StudentFactory)
+    jwt = moduleRef.get(JwtService)
 
-    await app.init();
-  });
+    await app.init()
+  })
 
-  test("[POST] /attachments", async () => {
-    const user = await studentFactory.makePrismaStudent();
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent()
 
-    const accessToken = jwt.sign({ sub: user.id.toString() });
+    const accessToken = jwt.sign({ sub: user.id.toString() })
 
     const response = await request(app.getHttpServer())
-      .post("/attachments")
-      .set("Authorization", `Bearer ${accessToken}`)
-      .attach("file", "./test/e2e/sample-upload.png");
+      .post('/attachments')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/sample-upload.png')
 
-    expect(response.statusCode).toBe(201);
-  });
-});
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    })
+  })
+})
--- src/infra/http/controllers/upload-attachment.controller.ts
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -1,4 +1,7 @@
+import { InvalidAttachmentType } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type'
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
 import {
+  BadRequestException,
   Controller,
   FileTypeValidator,
   MaxFileSizeValidator,
@@ -6,15 +9,17 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
-} from "@nestjs/common";
-import { FileInterceptor } from "@nestjs/platform-express";
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
 
-@Controller("/attachments")
+@Controller('/attachments')
 export class UploadAttachmentController {
-  //   constructor() {}
+  constructor(
+    private uploadAndCreateAttachment: UploadAndCreateAttachmentUseCase
+  ) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor("file"))
+  @UseInterceptors(FileInterceptor('file'))
   async handle(
     @UploadedFile(
       new ParseFilePipe({
@@ -23,13 +28,34 @@ export class UploadAttachmentController {
             maxSize: 1024 * 1024 * 2, // 2mb
           }),
           new FileTypeValidator({
-            fileType: ".(png|jpg|jpeg|pdf)",
+            fileType: '.(png|jpg|jpeg|pdf)',
           }),
         ],
       })
     )
     file: Express.Multer.File
   ) {
-    console.log(file);
+    const result = await this.uploadAndCreateAttachment.execute({
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case InvalidAttachmentType:
+          throw new BadRequestException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { attachment } = result.value
+
+    return {
+      attachmentId: attachment.id.toString(),
+    }
   }
 }
