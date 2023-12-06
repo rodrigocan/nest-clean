eadc1ed780c0c92fcf5de045537382ce0775139f
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-06 10:38:46 -0300

    Controller: Deletar Comentário da Resposta

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,39 +1,41 @@
-import { Either, left, right } from '@/core/either'
-import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
-import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
-import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { Either, left, right } from "@/core/either";
+import { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
+import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error";
+import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
+import { Injectable } from "@nestjs/common";
 
 interface DeleteAnswerCommentUseCaseRequest {
-  authorId: string
-  answerCommentId: string
+  authorId: string;
+  answerCommentId: string;
 }
 
 type DeleteAnswerCommentUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
   null
->
+>;
 
+@Injectable()
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
   async execute({
     authorId,
-    answerCommentId
+    answerCommentId,
   }: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
     const answerComment = await this.answerCommentsRepository.findById(
       answerCommentId
-    )
+    );
 
     if (!answerComment) {
-      return left(new ResourceNotFoundError())
+      return left(new ResourceNotFoundError());
     }
 
     if (answerComment.authorId.toString() !== authorId) {
-      return left(new NotAllowedError())
+      return left(new NotAllowedError());
     }
 
-    await this.answerCommentsRepository.delete(answerComment)
+    await this.answerCommentsRepository.delete(answerComment);
 
-    return right(null)
+    return right(null);
   }
 }
