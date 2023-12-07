559930e7c5a35192a22f7b2305afe5e2732153d5
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-07 13:21:09 +0000

    Editando perguntas com anexos

--- src/infra/http/controllers/edit-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -1,64 +1,109 @@
-import { AppModule } from '@/infra/app.module'
-import { DatabaseModule } from '@/infra/database/database.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { INestApplication } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
-import { Test } from '@nestjs/testing'
-import request from 'supertest'
-import { QuestionFactory } from 'test/factories/make-question'
-import { StudentFactory } from 'test/factories/make-student'
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
+import { QuestionFactory } from "test/factories/make-question";
+import { QuestionAttachmentFactory } from "test/factories/make-question-attachment";
+import { StudentFactory } from "test/factories/make-student";
 
-describe('Edit question (E2E)', () => {
-  let app: INestApplication
-  let studentFactory: StudentFactory
-  let questionFactory: QuestionFactory
-  let prisma: PrismaService
-  let jwt: JwtService
+describe("Edit question (E2E)", () => {
+  let app: INestApplication;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
+  let attachmentFactory: AttachmentFactory;
+  let questionAttachmentFactory: QuestionAttachmentFactory;
+  let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
-    }).compile()
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        AttachmentFactory,
+        QuestionAttachmentFactory,
+      ],
+    }).compile();
 
-    app = moduleRef.createNestApplication()
+    app = moduleRef.createNestApplication();
 
-    studentFactory = moduleRef.get(StudentFactory)
-    questionFactory = moduleRef.get(QuestionFactory)
-    prisma = moduleRef.get(PrismaService)
-    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory);
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
 
-    await app.init()
-  })
+    await app.init();
+  });
 
-  test('[PUT] /questions/:id', async () => {
-    const user = await studentFactory.makePrismaStudent()
+  test("[PUT] /questions/:id", async () => {
+    const user = await studentFactory.makePrismaStudent();
 
-    const accessToken = jwt.sign({ sub: user.id.toString() })
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
 
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
-    })
+    });
 
-    const questionId = question.id.toString()
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment1.id,
+      questionId: question.id,
+    });
+
+    await questionAttachmentFactory.makePrismaQuestionAttachment({
+      attachmentId: attachment2.id,
+      questionId: question.id,
+    });
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment();
+
+    const questionId = question.id.toString();
 
     const response = await request(app.getHttpServer())
       .put(`/questions/${questionId}`)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set("Authorization", `Bearer ${accessToken}`)
       .send({
-        title: 'New title',
-        content: 'New content',
-      })
+        title: "New title",
+        content: "New content",
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
+      });
 
-    expect(response.statusCode).toBe(204)
+    expect(response.statusCode).toBe(204);
 
     const questionOnDatabase = await prisma.question.findFirst({
       where: {
-        title: 'New title',
-        content: 'New content',
+        title: "New title",
+        content: "New content",
       },
-    })
+    });
 
-    expect(questionOnDatabase).toBeTruthy()
-  })
-})
+    expect(questionOnDatabase).toBeTruthy();
+
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    });
+
+    expect(attachmentsOnDatabase).toHaveLength(2);
+    expect(attachmentsOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: attachment1.id.toString(),
+        }),
+        expect.objectContaining({
+          id: attachment3.id.toString(),
+        }),
+      ])
+    );
+  });
+});
--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -5,23 +5,24 @@ import {
   HttpCode,
   Param,
   Put,
-} from '@nestjs/common'
-import { CurrentUser } from '@/infra/auth/current-user-decorator'
-import { UserPayload } from '@/infra/auth/jwt.strategy'
-import { z } from 'zod'
-import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+} from "@nestjs/common";
+import { CurrentUser } from "@/infra/auth/current-user-decorator";
+import { UserPayload } from "@/infra/auth/jwt.strategy";
+import { z } from "zod";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { EditQuestionUseCase } from "@/domain/forum/application/use-cases/edit-question";
 
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
-})
+  attachments: z.array(z.string().uuid()),
+});
 
-const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema)
+const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema);
 
-type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
+type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>;
 
-@Controller('/questions/:id')
+@Controller("/questions/:id")
 export class EditQuestionController {
   constructor(private editQuestion: EditQuestionUseCase) {}
 
@@ -30,21 +31,21 @@ export class EditQuestionController {
   async handle(
     @Body(bodyValidationPipe) body: EditQuestionBodySchema,
     @CurrentUser() user: UserPayload,
-    @Param('id') questionId: string
+    @Param("id") questionId: string
   ) {
-    const { title, content } = body
-    const userId = user.sub
+    const { title, content, attachments } = body;
+    const userId = user.sub;
 
     const result = await this.editQuestion.execute({
       title,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
       questionId,
-    })
+    });
 
     if (result.isLeft()) {
-      throw new BadRequestException()
+      throw new BadRequestException();
     }
   }
 }
