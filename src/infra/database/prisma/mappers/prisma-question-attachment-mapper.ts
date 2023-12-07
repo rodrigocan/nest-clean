dd6694f3e296d59b8b3c7a7d20ae26f715e136fb
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-07 12:54:47 +0000

    Criando pergunta com anexos

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,11 +1,11 @@
-import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { Prisma, Attachment as PrismaAttachment } from "@prisma/client";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 
 export class PrismaQuestionAttachmentMapper {
   static toDomain(raw: PrismaAttachment): QuestionAttachment {
     if (!raw.questionId) {
-      throw new Error('Invalid attachment type.')
+      throw new Error("Invalid attachment type.");
     }
 
     return QuestionAttachment.create(
@@ -14,15 +14,15 @@ export class PrismaQuestionAttachmentMapper {
         questionId: new UniqueEntityID(raw.questionId),
       },
       new UniqueEntityID(raw.id)
-    )
+    );
   }
 
   static toPrismaUpdateMany(
     attachments: QuestionAttachment[]
   ): Prisma.AttachmentUpdateManyArgs {
     const attachmentIds = attachments.map((attachment) => {
-      return attachment.id.toString()
-    })
+      return attachment.attachmentId.toString();
+    });
 
     return {
       where: {
@@ -33,6 +33,6 @@ export class PrismaQuestionAttachmentMapper {
       data: {
         questionId: attachments[0].questionId.toString(),
       },
-    }
+    };
   }
 }
--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -1,54 +1,69 @@
-import { AppModule } from '@/infra/app.module'
-import { DatabaseModule } from '@/infra/database/database.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
-import { INestApplication } from '@nestjs/common'
-import { JwtService } from '@nestjs/jwt'
-import { Test } from '@nestjs/testing'
-import request from 'supertest'
-import { StudentFactory } from 'test/factories/make-student'
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { AttachmentFactory } from "test/factories/make-attachment";
+import { StudentFactory } from "test/factories/make-student";
 
-describe('Create question (E2E)', () => {
-  let app: INestApplication
-  let studentFactory: StudentFactory
-  let prisma: PrismaService
-  let jwt: JwtService
+describe("Create question (E2E)", () => {
+  let app: INestApplication;
+  let studentFactory: StudentFactory;
+  let attachmentFactory: AttachmentFactory;
+  let prisma: PrismaService;
+  let jwt: JwtService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
-    }).compile()
+      providers: [StudentFactory, AttachmentFactory],
+    }).compile();
 
-    app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
-    studentFactory = moduleRef.get(StudentFactory)
+    app = moduleRef.createNestApplication();
 
-    jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
 
-    await app.init()
-  })
+    await app.init();
+  });
 
-  test('[POST] /questions', async () => {
-    const user = await studentFactory.makePrismaStudent()
+  test("[POST] /questions", async () => {
+    const user = await studentFactory.makePrismaStudent();
 
-    const accessToken = jwt.sign({ sub: user.id.toString() })
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
 
     const response = await request(app.getHttpServer())
-      .post('/questions')
-      .set('Authorization', `Bearer ${accessToken}`)
+      .post("/questions")
+      .set("Authorization", `Bearer ${accessToken}`)
       .send({
-        title: 'New question',
-        content: 'Question content',
-      })
+        title: "New question",
+        content: "Question content",
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
+      });
 
-    expect(response.statusCode).toBe(201)
+    expect(response.statusCode).toBe(201);
 
     const questionOnDatabase = await prisma.question.findFirst({
       where: {
-        title: 'New question',
+        title: "New question",
       },
-    })
+    });
 
-    expect(questionOnDatabase).toBeTruthy()
-  })
-})
+    expect(questionOnDatabase).toBeTruthy();
+
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    });
+
+    expect(attachmentsOnDatabase).toHaveLength(2);
+  });
+});
