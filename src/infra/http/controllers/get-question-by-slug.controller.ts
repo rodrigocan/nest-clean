733d4f1026797bc73c7003cceeabbc5e8bb3e5dc
Rodrigo Cândido <rodrigocan@gmail.com>
2023-12-09 20:00:26 -0300

    Prisma e Controller detalhe da pergunta

--- src/infra/http/controllers/get-question-by-slug.controller.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
-import { QuestionPresenter } from '../presenters/question-presenter'
+import { QuestionDetailsPresenter } from '../presenters/question-details-presenter'
 
 @Controller('/questions/:slug')
 export class GetQuestionBySlugController {
@@ -9,13 +9,13 @@ export class GetQuestionBySlugController {
   @Get()
   async handle(@Param('slug') slug: string) {
     const result = await this.getQuestionBySlug.execute({
-      slug
+      slug,
     })
 
     if (result.isLeft()) {
       throw new BadRequestException()
     }
 
-    return { question: QuestionPresenter.toHTTP(result.value.question) }
+    return { question: QuestionDetailsPresenter.toHTTP(result.value.question) }
   }
 }
